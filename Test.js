09beb476217399082036bbf7235069b57b798450
Angie C <a.cajusol98@gmail.com>
2025-02-09 14:22:14 +0100

    years in 20 years

--- Test.js
+++ Test.js
@@ -4,8 +4,10 @@ let age = input("Enter your age: ");
 
 let ageIn5Years = age + 5;
 let ageIn10Years = age + 10;
+let ageIn20Years = age + 20;
 
 console.log("Hello, " + userName + "!");
 console.log("You are " + age + " years old.");
 console.log("In 5 years, you will be " + ageIn5Years + " years old.");
 console.log("In 10 years, you will be " + ageIn10Years + " years old.");
+console.log("In 20 years, you will be " + ageIn20Years + " years old.");
