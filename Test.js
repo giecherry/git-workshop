120b0fe29cd340a5bfee6c9c2b556930d259bf2d
Angie C <a.cajusol98@gmail.com>
2025-02-09 14:16:50 +0100

    initial commit

---
+++ Test.js
@@ -0,0 +1,11 @@
+let userName = input("Enter your name: ");
+
+let age = input("Enter your age: ");
+
+let ageIn5Years = age + 5;
+let ageIn10Years = age + 10;
+
+console.log("Hello, " + userName + "!");
+console.log("You are " + age + " years old.");
+console.log("In 5 years, you will be " + ageIn5Years + " years old.");
+console.log("In 10 years, you will be " + ageIn10Years + " years old.");
